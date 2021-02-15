bd8f02d67489e548b07ecc48911d9a591ba25108
Shin Anase <F8hs02a1kX@gmail.com>
2021-02-15 21:27:33 +0900

    20210215

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from .forms import  inquiryForm
+import logging
+from django.urls import reverse_lazy
+from django.core.mail import EmailMessage
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
@@ -8,3 +11,29 @@ class IndexView(generic.TemplateView):
 class inquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = inquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_mail()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+
+    def send_mail(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = 'お問い合わせ{}'.format(title)
+        message = '送信者名：{0}\nメールアドレス：{1}\nメッセージ：\n{2}'.format(name, email, message)
+        from_email = 'admin@example.com'
+        to_list = [
+            'test@example.com'
+        ]
+        cc_list = [
+            email
+        ]
+
+        message = EmailMessage(subject=subject, body=message,
+                               from_email=from_email, to=to_list, cc=cc_list)
+        message.send()
