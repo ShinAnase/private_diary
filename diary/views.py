564b491ffc849447654ed4049030be1f9f3dfa61
Shin Anase <F8hs02a1kX@gmail.com>
2021-03-24 08:09:43 +0900

    20210324

--- diary/views.py
+++ diary/views.py
@@ -5,6 +5,9 @@ import logging
 from django.urls import reverse_lazy
 from django.contrib import messages
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Diary
+
 logger = logging.getLogger(__name__)
 
 
@@ -21,4 +24,13 @@ class inquiryView(generic.FormView):
         messages.success(self.request, 'メッセージ送信完了')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = 'diary_list.html'
+
+    def get_queryset(self):
+        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at') #作成日時降順
+        return diaries
 
